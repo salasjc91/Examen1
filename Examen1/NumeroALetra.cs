79b64c5c88716de83c933b6193e50f4363be31c9
salasjc <salasjc@HRD6402574>
2024-02-17 03:54:41 -0600

    Add project files.

---
+++ Examen1/NumeroALetra.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examen1
+{
+    class NumeroALetra
+    {
+        public string Convertir(int numero)
+        {
+            /*
+             * el switch se encarga de cambiar de numeros a letras
+             * 
+             */
+            switch (numero)
+            {
+                case 1:
+                    return "UNO";
+                case 2:
+                    return "DOS";
+                case 3:
+                    return "TRES";
+                case 4:
+                    return "CUATRO";
+                case 5:
+                    return "CINCO";
+                case 6:
+                    return "SEIS";
+                case 7:
+                    return "SIETE";
+                case 8:
+                    return "OCHO";
+                case 9:
+                    return "NUEVE";
+                default:
+                    return "";
+            }
+        }
+    }
+}
---
+++ Examen1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace Examen1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // se solicita al usuario que ingrese un numero del 1 al 9:
+
+            Console.WriteLine("Ingrese un número del 1 al 9:");
+            int numero = Convert.ToInt32(Console.ReadLine());
+
+            // en esta funcion se hace la conversion de numero a letras, utilizando la clase NumeroAletra.
+
+
+            NumeroALetra numeroALetra = new NumeroALetra();
+            string valorEnLetras = numeroALetra.Convertir(numero);
+
+
+            /*Utilice  el ciclo IF para verificar los datos ingresados por los usuarios
+             * en la funcion de Obtener Valores en letras para ver si estan dentro o fuera del rango 
+             */
+
+            if (valorEnLetras != "")
+            {
+                //Muestra el numero seleccionado al igual en la manera que se escribe.
+
+                Console.WriteLine($"El número que seleccionaste es: {numero} = {valorEnLetras}");
+            }
+            else
+            {
+                // Este mensaje indica que selecciono un numero fuera del rango.
+
+                Console.WriteLine("El número ingresado está fuera del rango permitido.");
+            }
+        }
+    }
+}
+
+        
+       
